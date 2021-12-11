7eeea14567888e44afb36458c39b01219ec62fd3
Damián Chocrón <damian@nimioestudio.com>
2021-12-11 18:24:59 +0100

    feat(SudokuService): create getColumn()

--- src/Services/SudokuService.php
+++ src/Services/SudokuService.php
@@ -9,5 +9,16 @@ class SudokuService
         return $board[$rowPosition];
     }
 
+    /* Given a board and column position, we iterate the rows to return a column */
+    public function getColumn($board, $columnPosition)
+    {
+
+        $columnCells = [];
+        for ($row = 0; $row < 9; $row++) {
+            array_push($columnCells, $board[$row][$columnPosition]);
+        }
+
+        return $columnCells;
+    }
     }
 }
