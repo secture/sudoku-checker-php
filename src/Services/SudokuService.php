29e47c0817232be378a3a9b481192cc8d81f0264
Damián Chocrón <damian@nimioestudio.com>
2021-12-11 18:44:49 +0100

    feat(SudokuService): create squareCoordinates

--- src/Services/SudokuService.php
+++ src/Services/SudokuService.php
@@ -4,6 +4,19 @@ namespace Secture\Service;
 
 class SudokuService
 {
+    /* Define the diferents square positions */
+    private $squareCoordinates = [
+        [1, 1, 1, 2, 2, 2, 3, 3, 3],
+        [1, 1, 1, 2, 2, 2, 3, 3, 3],
+        [1, 1, 1, 2, 2, 2, 3, 3, 3],
+        [4, 4, 4, 5, 5, 5, 6, 6, 6],
+        [4, 4, 4, 5, 5, 5, 6, 6, 6],
+        [4, 4, 4, 5, 5, 5, 6, 6, 6],
+        [7, 7, 7, 8, 8, 8, 9, 9, 9],
+        [7, 7, 7, 8, 8, 8, 9, 9, 9],
+        [7, 7, 7, 8, 8, 8, 9, 9, 9]
+    ];
+
     public function getRow($board, $rowPosition)
     {
         return $board[$rowPosition];
