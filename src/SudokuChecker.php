71a1e9545ea015c49adbc786d562b6ce8d6d8bc4
Damián Chocrón <damian@nimioestudio.com>
2021-12-14 00:02:01 +0100

    feat(SudokuChecker): implement SudokuService

--- src/SudokuChecker.php
+++ src/SudokuChecker.php
@@ -1,9 +1,29 @@
-<?php declare(strict_types=1);
+<?php
+
+declare(strict_types=1);
 
 namespace Secture\Sudoku;
 
+
+use Secture\Sudoku\SudokuService;
+
 final class SudokuChecker
 {
-    public function check()
-    {}
+    public function check($board)
+    {
+        $sudokuServiceInstance = new SudokuService();
+
+        $sudokuOriginalBoard = $sudokuServiceInstance->printBoard($board);
+        $sudokuResolved = $sudokuServiceInstance->resolve($board);
+
+        echo $sudokuOriginalBoard;
+        echo "<br>";
+
+        if ($sudokuResolved == false) {
+            echo 'Este Sudoku no tiene solucion';
+            return false;
+        }
+        
+        echo $sudokuServiceInstance->printBoard($sudokuResolved);
+    }
 }
