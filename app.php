ea77cd8a50b27edf8ca60e512aca696058ae1992
Damián Chocrón <damian@nimioestudio.com>
2021-12-13 23:17:52 +0100

    feat(app): implement SudokuGenerator

--- app.php
+++ app.php
@@ -3,6 +3,11 @@
 require_once(__DIR__ . '/vendor/autoload.php');
 
 use Secture\Sudoku\SudokuChecker;
+use Secture\Sudoku\SudokuGenerator;
+
+/* Here we generate a sudoku. You can set to 9 level difficulty.*/
+$sudokuGeneratorInstance = new sudokuGenerator(1);
+$sudoku  = $sudokuGeneratorInstance->puzzle();
 
 $checker = new SudokuChecker();
 
